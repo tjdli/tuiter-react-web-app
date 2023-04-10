ecf9ba7a6f856f354a76ab367916bba2ff1e4f68
tjdli <tristen.jd.li@gmail.com>
2023-04-09 23:00:14 -0400

    Fixed tuitThunk import

--- src/tuiter/tuits/tuit-item.js
+++ src/tuiter/tuits/tuit-item.js
@@ -1,7 +1,7 @@
 import React from "react";
 import TuitStats from "./tuit-stats";
 import { useDispatch } from "react-redux";
-import {deleteTuitThunk} from "./tuits-reducer";
+import {deleteTuitThunk} from "../../services/tuits-thunks";
 
 const TuitItem = ({ tuit }) => {
   const dispatch = useDispatch();
